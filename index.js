e463e2729c9fe7ae3da1f7248cb39f7635c00969
Manish Kumar <k88.manish@gmail.com>
2022-03-14 16:02:23 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+/* eslint-env node */
+"use strict";
+
+var path = require("path");
+
+// Empty extension takes advantage of Node's default require behavior to check for
+// eslint-local-rules.js as well as an eslint-local-rules folder with an index.js
+var exts = ["", ".cjs"];
+var rules = requireUp("tools/eslint-rules", __dirname);
+
+if (!rules) {
+  throw new Error(
+    "eslint-plugin-brikl-eslint-local: " +
+      'Cannot find "tools/eslint-rules{' +
+      [".js"].concat(exts.filter(Boolean)) +
+      "} " +
+      'or tools/eslint-rules/index.js (checked all ancestors of "' +
+      __dirname +
+      '").'
+  );
+}
+
+module.exports = {
+  rules: rules,
+};
+
+function requireUp(filename, cwd) {
+  var filepath = path.resolve(cwd, filename);
+
+  for (var i = 0; i < exts.length; i++) {
+    try {
+      return require(filepath + exts[i]);
+    } catch (error) {
+      // Ignore OS errors (will recurse to parent directory)
+      if (error.code !== "MODULE_NOT_FOUND") {
+        throw error;
+      }
+    }
+  }
+
+  var dir = path.dirname(cwd);
+  if (dir === cwd) {
+    return undefined;
+  }
+
+  return requireUp(filename, dir);
+}
